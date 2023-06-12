aa01ccddb15bd161795f4a3b0b9018661cbed468
Flávio Leandro Pirola <68715389+Flavio-LP@users.noreply.github.com>
2023-06-12 16:56:58 -0300

    Update server.js
    port 8000

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) { //allow cross origin requests
     next();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => { console.log(`Rodando na porta ${PORT}`) })
 
