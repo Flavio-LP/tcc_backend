13c2afce1aef136b4cd55de373df92e840e524c4
Flávio Leandro Pirola <68715389+Flavio-LP@users.noreply.github.com>
2023-06-12 18:46:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(function (req, res, next) { //allow cross origin requests
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
     //res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Origin", "https://tccreact-production.up.railway.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Origin", "*");
    //res.header("Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
